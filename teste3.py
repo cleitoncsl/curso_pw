1c7dc114649968eef963ab67703f2f9653a30af2
cleitoncsl <cleitoncsl@gmail.com>
2023-11-04 21:57:20 -0300

    curso playwright

--- teste3.py
+++ teste3.py
@@ -1,6 +1,8 @@
+import pytest
 from playwright.sync_api import Playwright, sync_playwright, expect
 
 
+@pytest.fixture(scope="function", autouse=True)
 def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
@@ -13,17 +15,12 @@ def run(playwright: Playwright) -> None:
     # page.get_by_test_id("emailAuth").get_by_label("Email").fill("symon.storozhenko@gmail.com")
     page.fill('input:below(:text("Email"))', "symon.storozhenko@gmail.com")
     # page.pause()
-    page.get_by_test_id("emailAuth").get_by_label("Email").press("Tab")
     page.get_by_label("Password").fill("test123")
-    page.get_by_label("Password").press("Tab")
-    page.get_by_test_id("forgotPasswordDesktop").press("Tab")
     page.get_by_test_id("submit").get_by_test_id("buttonElement").click()
     page.wait_for_load_state("networkidle")
-    expect(page.get_by_role("button", name="Log Ins")).to_be_hidden()
-    links = page.get_by_role("link").all()
-    for link in links:
-        a = link.text_content()
-        print(a)
+    #expect(page.get_by_role("button", name="Log Ins")).to_be_hidden()
+    assert page.is_visible("[aria-label=\"symon.storozhenko account menu\"]")
+
 
     # ---------------------
     context.close()
